cefbf0857acec7b851031832dc14569fa5933515
weixs <1729529437@qq.com>
2025-04-17 10:58:54 +0800

    app和main文件新建

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <!-- html -->
+     <div>
+        <h1>
+            你好啊！
+        </h1>
+     </div>
+</template>
+
+<script lang="ts">
+    // js
+    export default {
+        name: 'App',// 组件名
+
+    }
+</script>
+<style>
+    /* css */
+    .app{
+        background-color: #ddd;
+        box-shadow: 0 0 10px;
+        border-radius: 10px;
+        padding: 20px;
+    }
+</style>
