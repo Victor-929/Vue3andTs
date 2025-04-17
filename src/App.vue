c58d2e59dd0bc929e338c29830c4672db0b17236
weixs <1729529437@qq.com>
2025-04-17 17:33:58 +0800

    更新

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,22 @@
 <template>
     <!-- html -->
-     <div>
-        <h1>
-            你好啊！
-        </h1>
-     </div>
+
+        <Person />
 </template>
 
 <script lang="ts">
-    // js
-    export default {
-        name: 'App',// 组件名
+// js
+import Person from './components/Person.vue'
 
-    }
+export default {
+    name: 'App',// 组件名
+    components: {
+        Person// 注册组件
+    },
+
+}
 </script>
 <style>
-    /* css */
-    .app{
-        background-color: #ddd;
-        box-shadow: 0 0 10px;
-        border-radius: 10px;
-        padding: 20px;
-    }
+/* css */
+
 </style>
---
+++ src/components/Person.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="person">
+        <h2>汽车信息：一辆{{ car.brand }}车，价值{{ car.price }}万</h2>
+        <button @click="changePrice">修改汽车价格</button>
+        <button @click="changeBrand">修改汽车品牌</button>
+        <button @click="changeCar">修改汽车</button>
+        <hr>
+        <h2>当前求和为：{{ sum }}</h2>
+        <button @click="changeSum">点我sum+1</button>
+    </div>
+   
+</template>
+
+
+<script setup lang="ts" name="Person234">
+import { reactive ,ref} from 'vue';
+
+//数据
+let car = reactive({brand:'奔驰',price:100})
+let sum = ref(0)
+
+function changePrice() {
+    car.price += 10
+}
+function changeBrand(){
+    car.brand = '保时捷'
+}
+function changeCar() {
+    Object.assign(car,{brand:'奥迪',price:130})
+}
+function changeSum() {
+    sum.value += 1
+}
+
+changePrice
+changeBrand
+</script>
+
+<style scoped>
+.person {
+    background-color: #ddd;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+}
+
+button {
+    margin: 0 5px;
+}
+li {
+    font-size: 20px;
+}
+</style>
